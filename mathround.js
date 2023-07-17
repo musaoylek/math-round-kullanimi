e6a9abd3bf7f7da000e4738802d14736fa857313
musaoylek <101888206+musaoylek@users.noreply.github.com>
2023-07-17 19:57:37 +0300

    mathround.js

---
+++ mathround.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>musaoylek math.round metodu kullanımı</title>
+</head>
+<body>
+	<script>
+		// Math.Round fonksiyonunun sayı yuvarlama işlemlerinde kullanılır.
+		
+		var sayi;
+		sayi= prompt("Lütfen bir sayı giriniz")
+		var ert = Math.round(sayi);
+		alert("Sayının yuvarlama değeri : " +ert);
+	</script>
+</body>
+</html>
